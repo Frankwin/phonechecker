034a971dc476a2bd2f46a4748471324fe654f9c2
Frankwin Hooglander <fhooglander@ameriben.com>
2017-08-23 21:26:47 -0600

    Add file folder location as parameter Add start date as parameter Read list of names and numbers from JSON file instead of hardcoded

--- PhoneChecker/Program.cs
+++ PhoneChecker/Program.cs
@@ -2,57 +2,85 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace PhoneChecker
 {
     public class Program
     {
-        static void Main()
+        private const int RequiredParameterCount = 1;
+        private const string UsageString = "PhoneChecked <path> [startDate]";
+
+        static void Main(string[] args)
         {
-            var files = Directory.GetFiles(@"C:\Files\Mya");
-            var numbers = new Dictionary<string, string>
+            if (args.Length < RequiredParameterCount)
             {
-                {"Amanda", "985-5660"},
-                {"Anella", "919-1030"},
-                {"Keara", "860-5948"},
-                {"Jazzy", "688-2884"}
-            };
-            
+                Console.WriteLine("At least 1 parameter is required.");
+                Console.WriteLine(UsageString);
+                return;
+            }
+
+            var fileFolder = args[0];
+            DateTime? startDate = null;
+
+            if (args.Length == 2)
+            {
+                startDate = DateTime.Parse(args[1]);
+            }
+
+            var files = Directory.GetFiles(fileFolder);
+
+            var numbers = ReadNumbers();
+
             foreach (var filename in files)
             {
-                var inputFile = new StreamReader(Path.Combine(@"C:\Files\Mya", filename));
-                var headerRow = new List<string>();
-                var dataRow = new List<List<string>>();
-                var method = filename.Contains("voice") ? "called" : "texted";
-
-                while (!inputFile.EndOfStream)
+                using (var inputFile = new StreamReader(Path.Combine(fileFolder, filename)))
                 {
-                    var buffer = inputFile.ReadLine();
-                    if (buffer != null && (buffer.StartsWith("Usage") || buffer.StartsWith("\"") ||
-                                           buffer.StartsWith("Total") || string.IsNullOrWhiteSpace(buffer))) continue;
+                    Console.WriteLine("Checking Numbers in " + filename);
 
-                    if (buffer != null && buffer.StartsWith("Date"))
+                    var headerRow = new List<string>();
+                    var dataRow = new List<List<string>>();
+                    var method = filename.Contains("voice") ? "called" : "texted";
+
+                    while (!inputFile.EndOfStream)
                     {
-                        headerRow = buffer.Split(',').ToList();
-                    }
+                        var buffer = inputFile.ReadLine();
+                        if (buffer != null && (buffer.StartsWith("Usage") || buffer.StartsWith("\"") ||
+                                               buffer.StartsWith("Total") ||
+                                               string.IsNullOrWhiteSpace(buffer))) continue;
 
-                    if (buffer != null)
-                    {
-                        dataRow.Add(buffer.Split(',').ToList());
-                    }
-                }
-                Console.WriteLine("Checking Numbers in " + filename);
-
-                foreach (var row in dataRow)
-                {
-                    var dateColumn = headerRow.FindIndex(r => r.Contains("Date"));
-                    var numberColumn = headerRow.FindIndex(r => r.Contains("Number"));
-
-                    foreach (var number in numbers)
-                    {
-                        if (row[numberColumn].Contains(number.Value) && DateTime.Parse(row[dateColumn]) >= new DateTime(2017, 08, 01))
+                        if (buffer != null && buffer.StartsWith("Date"))
                         {
-                            Console.WriteLine("On " + row[dateColumn] + " " + row[numberColumn] + " (" + number.Key + ") was " + method);
+                            headerRow = buffer.Split(',').ToList();
+                            continue;
+                        }
+
+                        if (buffer == null) continue;
+
+                        var line = buffer.Split(',').ToList();
+                        if (line.Count > 7)
+                        {
+                            // Combine City & State 
+                            line[2] = line[2].TrimStart('"') + ", " + line[3].TrimEnd('"');
+                            line.RemoveAt(3);
+                        }
+                        dataRow.Add(line);
+                    }
+
+                    foreach (var row in dataRow)
+                    {
+                        var dateColumn = headerRow.FindIndex(r => r.Contains("Date"));
+                        var numberColumn = headerRow.FindIndex(r => r.Contains("Number"));
+
+                        foreach (var number in numbers)
+                        {
+                            if (row[numberColumn].Contains(number.Value) &&
+                                (startDate == null || DateTime.Parse(row[dateColumn]) >= startDate))
+                            {
+                                Console.WriteLine("On " + row[dateColumn] + " " + row[numberColumn] + " (" +
+                                                  number.Key + ") was " + method);
+                            }
                         }
                     }
                 }
@@ -60,6 +88,20 @@ namespace PhoneChecker
             Console.WriteLine("Done checking");
             Console.WriteLine("Press <enter> to continue");
             Console.ReadLine();
+        }
+
+        private static Dictionary<string, string> ReadNumbers()
+        {
+            Dictionary<string, string> numbers;
+
+            using (var dataFile = new StreamReader("DataFiles/Numberlist.json"))
+            {
+                using (var reader = new JsonTextReader(dataFile))
+                {
+                    numbers = JToken.ReadFrom(reader).ToObject<Dictionary<string, string>>();
+                }
+            }
+            return numbers;
         }
     }
 }
